856798f7ef2fbb70c121ab6d49a2fbc7a76efa12
joaquinmzumelzu <joaquinmunoztrade@gmail.com>
2025-03-27 01:51:51 -0300

    Initial commit: GTRUMP content transferred to Celsius template

---
+++ src/utils/utils.ts
@@ -0,0 +1,6 @@
+export const CA = "5qRbUppMinfq5K7WJRziC3AvBHeiiaWxJs7oCDXKpump";
+export const x = '';
+export const tg = 'https://t.me/GTRUMP_On_SOL';
+export const dex = `https://dexscreener.com/solana/${CA}`;
+export const raydium = `https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${CA}&inputMint=sol&outputMint=${CA}`;
+export const jupiter = `https://jup.ag/swap/SOL-${CA}`;
