f9b7465487c1bb3421b1817557eaab3f9a9313fc
ciberneticatradingdev <ciberneticatrading@gmail.com>
2025-08-20 12:06:26 -0400

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
-export const CA = "5qRbUppMinfq5K7WJRziC3AvBHeiiaWxJs7oCDXKpump";
-export const x = '';
-export const tg = 'https://t.me/GTRUMP_On_SOL';
+export const CA = "CA WILL UPDATE NOW";
+export const x = 'https://x.com/memestockmkt777';
+export const xcommunity = 'https://x.com/i/communities/1958195341427102137';
+export const tg = 'https://x.com/memestockmkt777';
 export const dex = `https://dexscreener.com/solana/${CA}`;
 export const raydium = `https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${CA}&inputMint=sol&outputMint=${CA}`;
 export const jupiter = `https://jup.ag/swap/SOL-${CA}`;
